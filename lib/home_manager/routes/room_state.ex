3051d068855b4d09f8f5cd8f95cae03771610b3c
Lukáš Peťko <luke.petko@gmail.com>
2025-11-05 20:35:07 +0100

    fixing decoding

--- lib/home_manager/routes/room_state.ex
+++ lib/home_manager/routes/room_state.ex
@@ -11,10 +11,17 @@ defmodule HomeManager.Routes.RoomState do
          {:ok, state} <- fetch_room_state(room_id) do
       conn
       |> put_resp_header("content-type", "application/json")
-      |> resp(200, Jason.encode!(state))
+      |> resp(200, decode_result(state, room_name))
     else
       {:error, reason} ->
         send_resp(conn, 404, Jason.encode!(reason))
     end
+  end
+
+  defp decode_result(state, room_name) do
+    state
+    |> Map.take([:id, :mode, :scene, :ts])
+    |> Map.put(:room_name, room_name)
+    |> Jason.encode!()
   end
 end
